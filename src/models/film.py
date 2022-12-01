1828c4c7e87f3514e15ca533ba7466c9df3d5010
977alex <info@97799.ru>
2022-12-01 14:20:33 +0300

    правки по API

--- src/models/film.py
+++ src/models/film.py
@@ -1,17 +1,40 @@
-import orjson
+from typing import Optional
 
 from pydantic import BaseModel
 
+from models.genre import FilmGenre
+from models.mixin import BaseModelMixin, PaginationMixin
+from models.person import FilmPerson
 
-def orjson_dumps(v, *, default):
-    return orjson.dumps(v, default=default).decode()
 
-
-class Film(BaseModel):
+class ESFilm(BaseModel):
     id: str
     title: str
-    description: str
+    description: Optional[str] = None
+    imdb_rating: Optional[float] = None
+    genre: Optional[list[str]] = None
+    director: Optional[list[str]] = None
+    actors: Optional[list[dict[str, str]]] = None
+    writers: Optional[list[dict[str, str]]] = None
+    directors: Optional[list[dict[str, str]]] = None
 
-    class Config:
-        json_loads = orjson.loads
-        json_dumps = orjson_dumps
+
+class ListResponseFilm(BaseModelMixin):
+    """Schema for Film work list"""
+
+    title: str
+    imdb_rating: Optional[float] = None
+
+
+class DetailResponseFilm(ListResponseFilm):
+    """Schema for Film work detail"""
+
+    description: Optional[str] = None
+    genre: Optional[list[FilmGenre]] = []
+    actors: Optional[list[FilmPerson]] = []
+    writers: Optional[list[FilmPerson]] = []
+    directors: Optional[list[FilmPerson]] = []
+
+
+class FilmPagination(PaginationMixin):
+    films: list[ListResponseFilm] = []
